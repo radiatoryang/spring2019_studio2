930917b0375c5042982b41b5400b15ec9404c746
Robert Yang <yang.robert.w@gmail.com>
2019-02-09 16:38:02 -0500

    added week02_introToUnity... see SimpleTextGame.cs and CubeMove.cs example code, or download the project and open the scene files in Assets/Scenes/

---
+++ week02_introToUnity/Assets/scripts/CubeMove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// USAGE: put this on a Cube
+// PURPOSE: let player control Cube with WASD
+public class CubeMove : MonoBehaviour
+{
+    public float moveSpeed = 0.5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // hold down W to make Cube move forward
+        if ( Input.GetKey(KeyCode.W) ) {
+            transform.Translate(0f, 0f, moveSpeed);
+        }
+
+        // hold down S to make Cube move backward
+        if ( Input.GetKey(KeyCode.S) ) {
+            transform.Translate(0f, 0f, -moveSpeed);
+        }
+
+        // hold down D to make Cube move right
+        if ( Input.GetKey(KeyCode.D) ) {
+            transform.Translate(moveSpeed, 0f, 0f);
+        }
+
+        // hold down A to make Cube move left
+        if ( Input.GetKey(KeyCode.A) ) {
+            transform.Translate(-moveSpeed, 0f, 0f);
+        }
+
+    }
+}
---
+++ week02_introToUnity/Assets/scripts/SimpleTextGame.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // you need this to talk to Text UI
+
+// USAGE: put this on a Text object
+// INTENT: a simple text game where we hold down SPACE for 10 seconds
+public class SimpleTextGame : MonoBehaviour
+{
+    public Text myText; // assign in Inspector!
+    float myScore = 0f; // store how long I've pressed SPACE
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if player holds down SPACE, then do this:
+        if ( Input.GetKey( KeyCode.Space ) ) {
+            // add time to myScore
+            myScore += Time.deltaTime;
+
+            // Time.deltaTime is:
+            // "delta" = difference
+            // deltaTime = how long the frame was, in seconds
+            // 60 FPS means dT = 1/60
+
+            // concatenation = put strings together
+            myText.text = "MY SCORE: " + myScore.ToString();
+        }
+    }
+}
